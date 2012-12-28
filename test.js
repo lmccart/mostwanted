b8de483228af01353ca4037295e19094b705ae06
Lauren McCarthy <laurmccarthy@gmail.com>
2012-12-27 20:13:41 -0500

    update search

--- test.js
+++ test.js
@@ -4,14 +4,15 @@ var client = api.createClient()
 
 // Copy your preferred match search's url from the website.
 var matchOptions = {
-	searchUrl: 'http://www.okcupid.com/match?filter1=0,34&filter2=2,23,29&filter3=3,25&filter4=5,31536000&filter5=1,1&filter6=35,2&filter7=9,256&filter8=30,468&filter9=25,8000,10000&locid=0&timekey=1&matchOrderBy=SPECIAL_BLEND&custom_search=0&fromWhoOnline=0&mygender=m&update_prefs=1&sort_type=0&sa=1&using_saved_search='
+	searchUrl: 'http://www.okcupid.com/match?filter1=0,17&filter2=2,0,100&filter3=5,315360000&filter4=1,0&filter5=35,0&locid=0&timekey=1&matchOrderBy=RANDOM&custom_search=0&fromWhoOnline=0&mygender=f&update_prefs=1&sort_type=0&sa=1&using_saved_search=',
+	count: 100
 }
 
 var automatorOptions = {
 	matchOptions: matchOptions,
-	dryRun: false,
+	dryRun: true,
 	maxMessages: 100,
-	timeBetweenRequests: 30000
+	timeBetweenRequests: 1000
 }
 
 var message = 'How are you?'
